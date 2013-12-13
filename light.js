ad5cd8ab67ac827fdb9bc8f7edd9a7e59f161b8d
Enrique <ecarrillo@teknol.com.mx>
2013-12-12 18:54:17 -0700

    Lots of code for lens.js

--- light.js
+++ light.js
@@ -4,7 +4,147 @@
 
 var lens ={}
 
+lens.Vector3 = (function () {
+	var v=function (x,y,z){
+		this.x = x || 0;
+		this.y = y || 0;
+		this.z = z || 0;
+	}
 
+	v.prototype.length = function () {
+		return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+	}
+
+	v.prototype.normalize = function () {
+		var len = this.length();
+		this.x /= len;
+		this.y /= len;
+		this.z /= len;
+	}
+
+	v.prototype.add = function (v) {
+		this.x += v.x;
+		this.y += v.y;
+		this.z += v.z;
+	}
+
+	v.prototype.sub = function (v) {
+		this.x -= v.x;
+		this.y -= v.y;
+		this.z -= v.z;
+	}
+
+	v.prototype.cross = function (v) {
+		var tx = this.y*v.z - this.z*v.y;
+		var ty = this.z*v.x - this.x*v.z;
+		var tz = this.x*v.y - this.y*v.x;
+		this.x=tx;
+		this.y=ty;
+		this.z=tz;
+	}
+
+	v.prototype.dot = function (v) {
+		return this.x*v.x + this.y*v.y + this.z*v.z;
+	}
+	return v;
+})();
+
+lens.Color = (function (){
+	var c = function (r,g,b,a) {
+		this.a = a || 0;
+		this.r = r || 0;
+		this.b = b || 0;
+		this.g = g || 0;
+	}
+
+	c.prototype.to255 = function () {
+		return 
+		[		
+			Math.min(this.r*255,255),
+			Math.min(this.g*255,255),
+			Math.min(this.b*255,255),
+			Math.min(this.a*255,255)
+		];
+	}
+
+	return c;
+})();
+
+lens.Sphere = (function () {
+	var sp = function (center, radius, color) {
+		this.center = center || new Vector3();
+		this.radius = radius || 0;
+		this.color = color || new Color();
+	};
+	return sp;
+})();
+
+lens.Camera = (function () {
+	var cam = function (left_bottom, left_top, right_top, eye) {
+		this.left_bottom = left_bottom || new Vector3();
+		this.left_top = left_top || new Vector3();
+		this.right_top = right_top || new Vector3();
+		this.eye = eye || new Vector3();
+	}
+
+	return cam;
+})();
+
+lens.Scene = (function (){
+	var scn = function (camera, objects) {
+		this.camera= camera || new Camera();
+		this.objects = objects || [];
+	}
+
+	scn.prototype.addObj = function (obj){
+		this.object.push(obj);
+	}
+
+	return scn;
+})();
+
+lens.Section = (function () {
+	var sct = function (left,top,right,bottom){
+		this.left = this.left || 0;
+		this.top = this.top || 0;
+		this.right = this.width || 0;
+		this.bottom = this.height ||0;
+	}
+
+	return sct;
+})();
+
+lens.Job = (function (){
+	var j = function (scene,section) {
+		this.scene = scene || new Scene();
+		this.section= section || new Section();
+	}
+
+	return j;
+})();
+
+lens.Renderer = (function () {
+	var rndr = function () {
+		this.job = new Job();
+	}
+
+	function getRay(x,y){
+
+	}
+
+	rndr.prototype.render = function (job) {
+		var j = this.job = job;
+
+		for(var y = j.top; y <= j.bottom; ++y){
+			for(var x = j.left; x <= j.right; ++x){
+				var ray = getRay(x,y);
+			}
+		}
+
+	}
+
+	return rndr;
+})();
 
 lens.render = function(done){
 	var buffer = [];
