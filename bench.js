43f36eedf1653e125999bc6123558d1bd0bfe420
Enrique <niofis@gmail.com>
2014-01-31 12:36:00 -0700

    :O

--- bench.js
+++ bench.js
@@ -1,9 +1,9 @@
 var spawn = require('child_process').spawn;
-/*
+
 function hrdiff(t1, t2) {
     var s = t2[0] - t1[0];
     var mms = t2[1] - t1[1];
-    return s+mms;//*1e9 + mms;
+    return s+(mms/1e9);//*1e9 + mms;
 }
 
 var hr_start = process.hrtime();
@@ -12,9 +12,4 @@ proc.on('close', function(code) {
 	var hr_end = process.hrtime();
 	console.log('light.js closed with code: ' + code);
 	console.log('time: ' + hrdiff(hr_start,hr_end) + " s");
-})*/
-var fs = require('fs');
-fs.readFile('/proc/cpuinfo', function (err,data) {
-	console.log(err);
-	console.log(data);
 })
